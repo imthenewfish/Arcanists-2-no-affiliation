8e1efec53c0b8849b28023982cd34d064628f56d
Ryan <ryanmfrost@gmail.com>
2018-05-26 22:01:31 -0700

    Arc 2 in 1080p

--- conf.lua
+++ conf.lua
@@ -1,8 +1,8 @@
 function love.comf(t)
-		t.title = "Test"
+		t.title = "Arc 2"
 		t.version "0.10.2"
-		t.window.width "1028"
-		t.window.height "720"
+		t.window.width "1920"
+		t.window.height "1080"
 		love.fps = 30
 		t.console = true
 end
