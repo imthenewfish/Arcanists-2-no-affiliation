528757004dd68f1bbbed3ea77a8618f1a581bde6
Ryan <ryanmfrost@gmail.com>
2018-05-26 21:58:36 -0700

    Initial commit

---
+++ conf.lua
@@ -0,0 +1,8 @@
+function love.comf(t)
+		t.title = "Test"
+		t.version "0.10.2"
+		t.window.width "1028"
+		t.window.height "720"
+		love.fps = 30
+		t.console = true
+end
---
+++ main.lua
@@ -0,0 +1,20 @@
+debug = true
+require ('lib/astray')
+require ('bin/30log-global')
+
+
+ 
+
+function love.load(dt)
+
+
+end
+
+function love.update(dt)
+
+
+end
+
+function love.draw(dt)
+
+end
